33e8e8310f7eaa867fcc3ac3ea784882c297f627
Carlos Sancho Ramírez <carlos.sancho.ramirez@gmail.com>
2017-07-25 12:50:06 +0200

    Correct JavaDoc warnings within the InputBitStream class

--- src/main/java/sword/bitstream/InputBitStream.java
+++ src/main/java/sword/bitstream/InputBitStream.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+/**
+ * Wrapper for a Java InputStream that adds functionality to read serialiazed content.
+ *
+ * This is a complementary class for {@link OutputBitStream}. Thus, this class
+ * provides lot of methods to read what the complementary class has written in
+ * to the output stream.
+ */
 public class InputBitStream implements Closeable {
 
     private final InputStream _is;
@@ -12,6 +19,10 @@ public class InputBitStream implements Closeable {
     private int _bitsOnBuffer;
     private boolean _closed;
 
+    /**
+     * Create a new instance wrapping the given InputStream.
+     * @param is InputStream used to read.
+     */
     public InputBitStream(InputStream is) {
         if (is == null) {
             throw new IllegalArgumentException();
@@ -20,6 +31,11 @@ public class InputBitStream implements Closeable {
         _is = is;
     }
 
+    /**
+     * Close this stream and the wrapped one.
+     * @throws IOException if it is not possible to close it.
+     *                     This is usually because it is already closed.
+     */
     @Override
     public void close() throws IOException {
         _is.close();
@@ -32,6 +48,21 @@ public class InputBitStream implements Closeable {
         }
     }
 
+    /**
+     * Read a single boolean from the stream.
+     *
+     * A byte has 8 bits and a boolean can be represented with a single bits.
+     * Thus, this method will only call {@link InputStream#read()} in the
+     * wrapped stream once every 8 calls to this method, until reading all
+     * bits from the previous read byte.
+     *
+     * This is a key method and all other more complex methods within the class
+     * depends on this.
+     *
+     * @return true or false depending on next bit value.
+     * @throws IOException if it is unable to read from the wrapped stream or
+     *                     this stream has been closed.
+     */
     public boolean readBoolean() throws IOException {
         assertNotClosed();
 
@@ -52,11 +83,13 @@ public class InputBitStream implements Closeable {
     }
 
     /**
-     * Reads a natural number (zero or positive integer) from the stream
+     * Read a natural number (zero or positive integer) from the stream
      * in the same format {@link OutputBitStream#writeNaturalNumber(long)} writes it.
      *
      * Ideally there is no upper limit for this number.
      * In reality it is currently limited by the 'long' boundaries.
+     * @return The read number
+     * @throws IOException if it is unable to read from the wrapped stream.
      */
     public long readNaturalNumber() throws IOException {
         long base = 0;
@@ -78,6 +111,8 @@ public class InputBitStream implements Closeable {
 
     /**
      * Read a single char from the stream
+     * @return the char read from the stream
+     * @throws IOException if it is unable to read from the wrapped stream.
      */
     public char readChar() throws IOException {
         return (char) readNaturalNumber();
@@ -87,6 +122,8 @@ public class InputBitStream implements Closeable {
      * Read a string of characters from the stream.
      * This method is complementary of {@link OutputBitStream#writeString(String)}.
      * Thus the original value given to that method should be returned here.
+     * @return the string of characters read from the stream.
+     * @throws IOException if it is unable to read from the wrapped stream.
      */
     public String readString() throws IOException {
         final int length = (int) readNaturalNumber();
@@ -98,6 +135,13 @@ public class InputBitStream implements Closeable {
         return str.toString();
     }
 
+    /**
+     * Read a symbol from the stream according to the given Huffman table.
+     * @param table Huffman table used to decode the symbol.
+     * @param <E> Type of the symbol to decode.
+     * @return The symbol found in the stream according too the Huffman table.
+     * @throws IOException if it is not possible to read from the stream.
+     */
     public <E> E readHuffmanSymbol(HuffmanTable<E> table) throws IOException {
         if (table == null) {
             throw new IllegalArgumentException();
@@ -124,8 +168,12 @@ public class InputBitStream implements Closeable {
     /**
      * Read a value assuming that the value can only
      * be inside a range of values.
+     * This is a complementary method for {@link OutputBitStream#writeRangedNumber(int, int, int)}.
+     *
      * @param min Minimum number allowed in the range (inclusive)
      * @param max Maximum number allowed in the range (inclusive)
+     * @return the decoded number read from the stream according to the provided range.
+     * @throws IOException if it is unable to read from the wrapped stream.
      */
     public int readRangedNumber(int min, int max) throws IOException {
         final int normMax = max - min;
@@ -163,11 +211,14 @@ public class InputBitStream implements Closeable {
     /**
      * Read a string of characters from the stream assuming that
      * the given sorted set of chars are the only possibilities
-     * that can be found.
+     * that can be found and that it is the same probability for
+     * each of the possibilities.
      *
      * @param charSet Array of char containing all possible
      *                characters that the string may contain
      *                in the same order it was given when encoded.
+     * @return The decoded string according to the charSet provided.
+     * @throws IOException if it is unable to read from the wrapped stream.
      */
     public String readString(char[] charSet) throws IOException {
         final int max = charSet.length - 1;
@@ -185,7 +236,9 @@ public class InputBitStream implements Closeable {
     /**
      * Read a Huffman table from the stream.
      * @param supplier Used to read each of the symbols from the stream.
+     * @param <E> Type of the decoded symbol expected in the Huffman table.
      * @return The HuffmanTable resulting of reading the stream.
+     * @throws IOException if it is unable to read from the wrapped stream.
      */
     public <E> HuffmanTable<E> readHuffmanTable(SupplierWithIOException<E> supplier) throws IOException {
         final ArrayList<Integer> levelLengths = new ArrayList<>();
@@ -211,6 +264,8 @@ public class InputBitStream implements Closeable {
 
     /**
      * Read a char-types Huffman table.
+     * @return The resulting Huffman table on reading and decoding from the stream.
+     * @throws IOException if it is unable to read from the wrapped stream.
      */
     public HuffmanTable<Character> readHuffmanCharTable() throws IOException {
         return readHuffmanTable(this::readChar);
